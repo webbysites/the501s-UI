5e5e2abd0e1ebd16d948777b4139782bc7e0c1de
Travis Vela <jtravisvela@gmail.com>
2020-01-15 23:10:41 -0600

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  datesAPI: 'http://http://the501s-api-prod.us-east-2.elasticbeanstalk.com/dates'
+  datesAPI: 'http://the501s-api-prod.us-east-2.elasticbeanstalk.com/dates'
 };
