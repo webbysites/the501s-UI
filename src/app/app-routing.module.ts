58a70dac64f8b3ad57c1b7d94ffa50e77c94d848
Travis Vela <jtravisvela@gmail.com>
2021-04-26 21:48:48 -0500

    update home and about page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -17,7 +17,7 @@ const routerOptions: ExtraOptions = {
 
 const routes: Routes = [
   {
-    path: '', component: LandingPageComponent
+    path: '', component: HomeComponent
   },
   {
     path: 'home', component: HomeComponent
